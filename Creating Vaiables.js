bbd7ee81d9310b4f5667f2482b5ddfb0e325748f
Abdul Rauf <114975361+armarath@users.noreply.github.com>
2023-01-11 18:13:49 +0500

    Add files via upload
    JKR Online Class 01-Assignment of Creating Variables

---
+++ Creating Vaiables.js
@@ -0,0 +1,44 @@
+//=========================================== Varable with let Keyword================================================//
+
+// Example 1
+
+let myName = "abdul rauf"; // created a variable with string
+console.log(myName); // output is abdul rauf
+
+// Example 2
+
+let myAge = 99; // created a variable withe number type
+console.log(myAge); // output is 99
+
+// Example 3
+
+myName = "abdul rauf marth"; // reassigned a new value to variable
+console.log(myName); // output is abdul rauf marth
+
+// Example 4
+
+myAge = 77; // reassigned a new value to variable
+console.log(myAge); // output is 77
+
+//=============================================== Varable with Const Keyword================================================//
+
+// Example 1
+
+const myName1 = "abdul rauf -1"; // created a variable with string
+console.log(myName1); // output is abdul rauf
+
+// Example 2
+
+const myAge1 = 909; // created a variable withe number type
+console.log(myAge1); // output is 99
+
+// Example 3
+
+myName1 = "abdul rauf marth-1"; // error bcz reassigning a new value to const variable is not allowed
+console.log(myName1); // output is abdul rauf marth
+
+// Example 4
+
+myAge1 = 707; // // error bcz reassigning a new value to const variable is not allowed
+console.log(myAge1); // output is 77
+
